c4ccdee97c447fc28b75f5fdd081c716d03aa798
rammfall <15bogdan17@gmail.com>
2022-08-05 22:08:02 +0300

    Fix

--- src/server.mjs
+++ src/server.mjs
@@ -2,7 +2,7 @@ import fastify from './index.mjs';
 
 (async () => {
   try {
-    await fastify.listen(process.env.PORT || 3000);
+    await fastify.listen(process.env.PORT || 3000, '0.0.0.0');
   } catch (err) {
     console.log(err);
   }
