60f9cfe018de2cc78e9304b0481cfe90a725bf44
rammfall <15bogdan17@gmail.com>
2022-08-05 20:37:38 +0300

    Complete task backend

--- src/index.js
+++ src/index.js
@@ -10,8 +10,48 @@ fastify.register(import('@fastify/multipart'), {
 });
 fastify.register(import('@fastify/cookie'));
 
-fastify.get('/hello', (request, reply) => {
-  return reply.send('world');
+const tasks = {
+
+};
+let ids = 1;
+
+fastify.post('/task', (request, reply) => {
+  const { name } = request.body;
+  const id = ids++;
+
+  tasks[id] = {
+    id,
+    name,
+    isCompleted: false,
+  }
+
+  reply.send(tasks);
+});
+
+fastify.get('/task', (request, reply) => {
+  reply.send(tasks);
+});
+
+fastify.patch('/task/isCompleted/:id', (request, reply) => {
+  const {params: { id }} = request;
+
+  tasks[id].isCompleted = !tasks[id].isCompleted;
+  reply.send(tasks);
+});
+
+fastify.patch('/task/:id', (request, reply) => {
+  const { params: { id }, body: { name } } = request;
+
+  tasks[id].name = name;
+  reply.send(tasks);
+});
+
+fastify.delete('/task/:id', (request, reply) => {
+  const { params: { id } } = request;
+
+  delete tasks[id];
+
+  reply.send(tasks);
 });
 
 export default fastify;
